27fd17741460e44bb5dd66f1d046aa2dc49b6a36
Marina Wahl <mari.wahl9@gmail.com>
2014-04-28 11:30:41 -0400

    gist

---
+++ src/further_examples/useful/version_info.py
@@ -0,0 +1,9 @@
+import sys 
+print "System version:",sys.version
+import numpy as np
+print "Numpy version:",np.version.version
+import matplotlib
+print "Matplotlib version:",matplotlib.__version__
+import pandas as pd
+print "Pandas version:",pd.__version__
+
