7b79f5816a0bcd85103610ff8572c2b8d56557b2
Bernardo Sulzbach <mafagafogigante@gmail.com>
2014-07-07 16:21:33 -0300

    Faster, better, stronger

--- src/further_examples/arrays_and_strings/reverse_str.py
+++ src/further_examples/arrays_and_strings/reverse_str.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
 # mari von steinkirch @2013
 # steinkirch at gmail
-
-   
-def reverse_str(s):
-    ''' in place '''
-    sr_ls = []
-    for i in range(len(s)-1, -1, -1):
-        sr_ls.append(s[i])
-    return ''.join(sr_ls)
         
-
+reverse_string = lambda s: s[::-1]
 
 
 def main():
     s1 = 'abcdefg'
     s2 = 'buffy'
     s3 = ''
-    print(reverse_str(s1))
-    print(reverse_str(s2))
-    print(reverse_str(s3))
+    print(reverse_string(s1))
+    print(reverse_string(s2))
+    print(reverse_string(s3))
                    
 if __name__ == '__main__':
     main()
