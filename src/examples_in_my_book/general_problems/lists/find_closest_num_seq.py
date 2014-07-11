dc3ffe1e5d9ea0e63fb5d47954a5f3acc209b921
Bernardo Sulzbach <mafagafogigante@gmail.com>
2014-07-11 17:09:05 -0300

    Fixes, reformats and a new file
    A typo corrected.
    More PEP 8.
    A file that compares randint with randrange.

--- src/examples_in_my_book/general_problems/lists/find_closest_num_seq.py
+++ src/examples_in_my_book/general_problems/lists/find_closest_num_seq.py
@@ -1,47 +1,57 @@
 #!/usr/bin/python3
-# mari von steinkirch @2013
-# steinkirch at gmail
+
+# Mari von Steinkirch @ 2013
+# mari.wahl9@gmail.com
 
 
+def find_closest_num_seq_unsorted(sequence):
+    """
+    Find the closest two numbers in a sequence.
+    If we do this without sorting first, the runtime will be O(n^2).
+    :param sequence: a sequence of numbers.
+    """
+    assert len(sequence) > 1, "Sequence must have at least 2 elements."
+    biggest_difference = float("inf")
+    for x in sequence:
+        for y in sequence:
+            if x == y:
+                continue
+            difference = abs(x - y)
+            if difference < biggest_difference:
+                best_x, best_y, biggest_difference = x, y, difference
+    return best_x, best_y
 
-def find_closest_num_seq_unsorted(seq):
-    ''' Find the Closest two Numbers in a Sequence. If we do this without sorting 
-        first, the runtime will be O(n^2) '''
-    dd = float("inf")
-    for x in seq:
-        for y in seq:
-            if x == y: continue
-            d = abs(x - y)
-            if d < dd:
-                xx, yy, dd = x, y, d	
-    return xx, yy
 
+def find_closest_num_seq_sorted(sequence):
+    """
+    Find the closest two numbers in a sequence.
+    However, if we sort first, we can achieve it with runtime O(n log n).
+    :param sequence: a sequence of numbers.
+    """
+    assert len(sequence) > 1, "Sequence must have at least 2 elements."
+    sequence.sort()
+    print(sequence)
+    biggest_difference = float("inf")
+    for i in range(len(sequence) - 1):
+        x, y = sequence[i], sequence[i + 1]
+        if x == y:
+            continue
+        difference = abs(x - y)
+        if difference < biggest_difference:
+            best_x, best_y, biggest_difference = x, y, difference
+    return best_x, best_y
 
-def find_closest_num_seq_sorted(seq):
-    ''' However, if we sort first, we can achieve it with runtime O(n log n): '''
-    seq.sort()
-    print(seq)
-    dd = float("inf")
-    for i in range(len(seq) - 1):
-        x, y = seq[i], seq[i+1]
-        if x == y: continue
-        d = abs(x-y)
-        if d < dd:
-            xx, yy, dd = x, y, d
-    return xx, yy
-		
 
 def test_find_closest_num_seq(module_name='this module'):
     import random
     seq = [random.randrange(100) for i in range(20)]
-    print(seq)	
+    print(seq)
     print(find_closest_num_seq_sorted(seq))
     print(find_closest_num_seq_unsorted(seq))
-           
-    s = 'Tests in {name} have {con}!'
+
+    s = 'Tests in {name} have {con}.'
     print(s.format(name=module_name, con='passed'))
 
 
 if __name__ == '__main__':
     test_find_closest_num_seq()
-
