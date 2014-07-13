7e64e1a2ad77cc4d572d16b0422a733fa79e9008
Bernardo Sulzbach <mafagafogigante@gmail.com>
2014-07-13 00:38:47 -0300

    Python 3 compatibility and major failure avoidance.
    passing_cmd_line_args.py now works with Python 3 too.
    
    "assert(merge_sorted_seqseq1, seq2) == sorted(seq3))"
    was isolated, headshoted and properly incinerated.
    
    Docs were also 'improved' (subjective, but less typos and proper
    capitalization are usually seen as improvements.)

--- src/examples_in_my_book/general_problems/modules/passing_cmd_line_args.py
+++ src/examples_in_my_book/general_problems/modules/passing_cmd_line_args.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-# mari von steinkirch @2013
-# steinkirch at gmail
+
+# Mari von Steinkirch @ 2013
+# mari.wahl9@gmail.com
 
 
 import sys
 
+
 def main():
-    ''' print command line arguments '''
+    """
+    Print command line arguments.
+    """
     for arg in sys.argv[1:]:
-        print arg
+        print(arg)
 
 if __name__ == "__main__":
     main()
