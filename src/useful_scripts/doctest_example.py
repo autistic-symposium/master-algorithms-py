84e8d428714b5348c14a762fec75cce26a1f1e68
bt3gl <mari.wahl9@gmail.com>
2014-11-26 13:34:23 -0500

    doctest simple example

---
+++ src/useful_scripts/doctest_example.py
@@ -0,0 +1,11 @@
+'''
+The doctest module automatically runs any statement begining with >>> 
+and compares the following line with the output from the interpreter.
+
+>>> 1 == 1
+False
+'''
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
