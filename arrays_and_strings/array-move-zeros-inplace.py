3b218fae015107b2ddbcea2f8257f8c2c5c4b026
bt3gl <138340846+bt3gl-google@users.noreply.github.com>
2023-07-30 15:54:34 -0700

    Update array-move-zeros-inplace.py

--- arrays_and_strings/array-move-zeros-inplace.py
+++ arrays_and_strings/array-move-zeros-inplace.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# author: bt3gl
+
+
 '''
 Given an integer array nums, move all 0's to the end of it while
 maintaining the relative order of the non-zero elements.
