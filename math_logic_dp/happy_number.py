48ce09742e2e089e469db0452c2762f02168ffeb
bt3gl <138340846+bt3gl-google@users.noreply.github.com>
2023-07-30 13:04:04 -0700

    Create happy_number.py

---
+++ math_logic_dp/happy_number.py
@@ -0,0 +1,19 @@
+def get_next(n):
+  
+  total_sum = 0
+  while n > 0:
+    n, digit = divmod(n, 10)
+    total_sum += digit**2
+
+  return total_sum
+
+
+def is_happy(self, n: int) -> bool:
+
+  seen = set()
+  while n != 1 and n not in seen:
+    seen.add(n)
+    n = get_next(n)
+
+  return n == 1
+    
