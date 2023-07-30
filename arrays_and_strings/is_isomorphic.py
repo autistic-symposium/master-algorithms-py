f2418e56c1e3223c35d7be75f2df10c67fadfcc9
bt3gl <138340846+bt3gl-google@users.noreply.github.com>
2023-07-30 14:13:02 -0700

    Create is_isomorphic.py

---
+++ arrays_and_strings/is_isomorphic.py
@@ -0,0 +1,15 @@
+def is_isomorphic(s: str, t: str) -> bool:
+        
+        map_s_to_t = {}
+        map_t_to_s = {}
+        
+        for ss, tt in zip(s, t):
+            
+            if (ss not in map_s_to_t) and (tt not in map_t_to_s):
+                map_s_to_t[ss] = tt
+                map_t_to_s[tt] = ss
+            
+            elif (map_s_to_t.get(ss) != tt) or (map_t_to_s.get(tt) != ss):
+                return False
+
+        return True
