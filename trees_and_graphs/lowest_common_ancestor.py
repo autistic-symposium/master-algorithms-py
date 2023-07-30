da0f3fd5d888bde8ba9bc85160fc17c076334724
bt3gl <138340846+cypher-bt3gl@users.noreply.github.com>
2023-07-29 17:41:24 -0700

    Create lowest_common_ancestor.py

---
+++ trees_and_graphs/lowest_common_ancestor.py
@@ -0,0 +1,25 @@
+'''
+Given a binary tree, find the lowest common ancestor (LCA) of two given nodes in the tree.
+'''
+
+class ThisTree:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+        def dfs(root, p, q):
+
+            if not root:
+                return False
+            
+            left = dfs(root.left, p, q)
+            right = dfs(root.right, p, q)
+            mid = root == p or root == q
+
+            if mid + left + right >= 2:
+                self.answer = root
+            
+            return left or right or mid
+                
+        dfs(root, p, q)
+        
+        return self.answer
+    
