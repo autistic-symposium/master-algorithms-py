d6fedcee532cfc0a9d890ff12f584eff3108a93b
marina <138340846+bt3gl-cryptographer@users.noreply.github.com>
2023-08-02 13:56:52 -0700

    Create k_element_stream.py

---
+++ heaps/k_element_stream.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# author: bt3gl
+
+
+class KthLargest:
+
+    def __init__(self, k, nums):
+
+        self.k = k
+        self.heap = nums
+        heapq.heapify(self.heap)
+
+        while len(self.heap) > k:
+            heapq.heappop(self.heap)
+        
+
+    def add(self, val: int) -> int:
+
+        heapq.heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        
+        return self.heap[0]
+        
+
+  
