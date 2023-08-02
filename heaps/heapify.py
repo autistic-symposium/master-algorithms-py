0588483a1902e31861d65bdb9ccfec9112ffbf66
marina <138340846+bt3gl-cryptographer@users.noreply.github.com>
2023-08-02 13:05:42 -0700

    Update heapify.py

--- heaps/heapify.py
+++ heaps/heapify.py
@@ -16,3 +16,15 @@ heapq.heapify(min_heap)
 
 max_heap = [-x for x in min_heap]
 heapq.heapify(max_heap)
+
+heapq.heappush(min_heap, 5)
+heapq.heappush(min_heap, -5)
+
+min_elem = min_heap[0]
+max_elem = -1 * max_heap[0
+
+heapq.heappop(min_heap)
+heapq.heappop(max_heap)
+
+size_min_heap = len(min_heap)
+size_max_heap = len(max_heap)
