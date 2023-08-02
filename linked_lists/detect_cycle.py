4e82aa60605c9b43a8a57b20654decee1f833e28
marina <138340846+bt3gl-cryptographer@users.noreply.github.com>
2023-08-02 16:39:40 -0700

    Create detect_cycle.py

---
+++ linked_lists/detect_cycle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# author: bt3gl
+
+
+def has_cycle(self, head) -> bool:
+        
+        if not head:
+            return False
+        
+        p1 = head
+        p2 = head.next
+        
+        while p1 != p2:
+            
+            if not p1 or not p2 or not p2.next:
+                return False
+            
+            p1 = p1.next
+            p2 = p2.next.next
+        
+        return True
+            
