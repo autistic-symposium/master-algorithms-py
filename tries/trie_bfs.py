feb21289dbc98e88a79ab25eda66406a49d9b2df
marina <138340846+bt3gl-cryptographer@users.noreply.github.com>
2023-08-03 16:59:19 -0700

    Create trie_bfs.py

---
+++ tries/trie_bfs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# author: bt3gl
+
+
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+
+
+def level_orders(root):
+
+    if root is None:
+        return []
+
+    result = []
+    queue = collections.deque([root])
+
+    while queue:
+        level = []
+        for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                queue.extend(node.children)
+            result.append(level)
+        
+  return result
