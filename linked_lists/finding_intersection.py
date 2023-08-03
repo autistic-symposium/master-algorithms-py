eb36c2249db66f8483a7f770a4dc9810a9c6cd5a
marina <138340846+bt3gl-cryptographer@users.noreply.github.com>
2023-08-02 17:14:46 -0700

    Create finding_intersection.py

---
+++ linked_lists/finding_intersection.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# author: bt3gl
+
+class Node:
+    def __init__(self, val):
+      self.val = val
+      self.next = None
+
+
+def get_intersection_node(self, head_a: Node, head_b: Node) -> Optional[ListNode]:
+        
+        seen_b = set()
+        
+        while head_b is not None:
+            
+            seen_b.add(head_b)
+            head_b = head_b.next
+
+        while head_a is not None:
+
+            if head_a in seen_b:
+                return head_a
+            
+            head_a = head_a.next
+            
