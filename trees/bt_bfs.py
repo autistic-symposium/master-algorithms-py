070071e687072ab3d0eb8c086b61898a7d8fb6e1
marina <138340846+bt3gl-cryptographer@users.noreply.github.com>
2023-08-03 13:20:29 -0700

    Update and rename tree_bfs.py to bt_bfs.py

--- trees/bt_bfs.py
+++ trees/bt_bfs.py
@@ -2,11 +2,8 @@
 # -*- coding: utf-8 -*-
 # author: bt3gl
 
-# Given the root of a binary tree, return the level order traversal of its nodes' values. 
-# (i.e., from left to right, level by level).
 
-
-def level_order(root: Optional[TreeNode]) -> list[list[int]]:
+def level_order(root: Optional[Node]) -> list[list[int]]:
         
         if root is None:
             return []
