3024f285e5f302bfe9c13241bba90fc2d6dca715
marina <138340846+bt3gl-cryptographer@users.noreply.github.com>
2023-08-07 15:48:48 -0700

    Update merge_two_lists.py

--- linked_lists/merge_two_lists.py
+++ linked_lists/merge_two_lists.py
@@ -3,13 +3,13 @@
 # author: bt3gl
 
 
-class ListNode:
+class Node:
   def __init__(self, val=0, next=None):
       self.val = val
       self.next = next
 
 
-def merge_two_list(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+def merge_two_list(list1: Optional[Node], list2: Optional[Node]) -> Optional[Node]:
         
         if not list1:
             return list2
@@ -23,4 +23,4 @@ def merge_two_list(list1: Optional[ListNode], list2: Optional[ListNode]) -> Opti
         else:
             list2.next = merge_two_list(list1, list2.next)
             return list2
-            
+          
