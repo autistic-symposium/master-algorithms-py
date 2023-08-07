69d068678bfcd4a3318d3328e9492659e7a76731
marina <138340846+bt3gl-cryptographer@users.noreply.github.com>
2023-08-07 15:54:37 -0700

    Update swap_every_two_nodes.py

--- linked_lists/swap_every_two_nodes.py
+++ linked_lists/swap_every_two_nodes.py
@@ -3,21 +3,20 @@
 # author: bt3gl
 
 
-class ListNode:
+class Node:
   def __init__(self, val=0, next=None):
       self.val = val
       self.next = next
 
 
-def swap_pairs(head: Optional[ListNode]) -> Optional[ListNode]:
+def swap_pairs(head: Optional[Node]) -> Optional[Node]:
+  
       if not head or not head.next:
           return head
 
-      ## nodes to be swapped
       first_node = head
       second_node = head.next
 
-      # swapping
       first_node.next = swap_pairs(second_node.next)
       second_node.next = first_node
 
