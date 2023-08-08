6dbd139af1e88f87bf8e9a2b91f55818a2921f38
marina <138340846+bt3gl-cryptographer@users.noreply.github.com>
2023-08-07 17:09:47 -0700

    Update playing_with_math.py

--- math/playing_with_math.py
+++ math/playing_with_math.py
@@ -8,7 +8,6 @@ import random
 
 
 def find_greatest_common_divider(a, b) -> int:
-    '''Implements the greatest common divider algorithm '''
     
     while(b != 0):
         result = b
@@ -18,7 +17,6 @@ def find_greatest_common_divider(a, b) -> int:
 
 
 def _is_prime(number) -> bool:
-    '''Check if a number is prime '''
 
     if number < 2:
         return False
@@ -31,21 +29,8 @@ def _is_prime(number) -> bool:
 
 
 def find_prime_factors(number) -> list:
-    '''Find prime factors of a number '''
     
     divisors = [d for d in range(2, number//2 + 1) if number % d == 0]
     primes = [d for d in divisors if _is_prime(d)]
 
     return primes
-
-
-
-if __name__ == '__main__':
-
-    n1 = 21
-    n2 = 7
-
-    print(f'Greatest common divider of {n1} and {n2} is {find_greatest_common_divider(n1, n2)}')
-    print(f'Prime factors of {n1} are {find_prime_factors(n1)}')
-
-
