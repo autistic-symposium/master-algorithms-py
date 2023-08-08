3d8b77bf80924b83ccb59ebfb8a4216682a63a4a
bt3gl <138340846+bt3gl-cryptographer@users.noreply.github.com>
2023-08-08 16:33:04 -0700

    Update heap_sort.py

--- heaps/heap_sort.py
+++ heaps/heap_sort.py
@@ -3,25 +3,26 @@
 # author: bt3gl
 
 
-def heap_sort(self, lst: List[int]) -> None:
+def heap_sort(self, array) -> list:
 
       def max_heapify(heap_size, index):
         
             left, right = 2 * index + 1, 2 * index + 2
             largest = index
         
-            if left < heap_size and lst[left] > lst[largest]:
+            if left < heap_size and array[left] > array[largest]:
                 largest = left
-            if right < heap_size and lst[right] > lst[largest]:
+            elif if right < heap_size and array[right] > array[largest]:
                 largest = right
-            if largest != index:
-                lst[index], lst[largest] = lst[largest], lst[index]
+            elif largest != index:
+                array[index], array[largest] = array[largest], array[index]
                 max_heapify(heap_size, largest)
 
       for i in range(len(lst) // 2 - 1, -1, -1):
-            max_heapify(len(lst), i)
+            max_heapify(len(array), i)
 
-      for i in range(len(lst) - 1, 0, -1):
-            lst[i], lst[0] = lst[0], lst[i]
+      for i in range(len(array) - 1, 0, -1):
+            array[i], array[0] = array[0], array[i]
             max_heapify(i, 0)
         
+      return array
