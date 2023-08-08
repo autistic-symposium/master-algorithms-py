e5efd5424a7faafd3a5506b288c825c9b6309aa7
bt3gl <138340846+bt3gl-cryptographer@users.noreply.github.com>
2023-08-08 14:43:04 -0700

    Update bt_construct_inorder_preorder.py

--- trees/bt_construct_inorder_preorder.py
+++ trees/bt_construct_inorder_preorder.py
@@ -2,24 +2,24 @@
 # -*- coding: utf-8 -*-
 # author: bt3gl
 
-def build_tree(preorder: list[int], inorder: list[int]) -> Optional[Node]:
+
+def build_tree(preorder, inorder) -> Optional[Node]:
         
-        def helper(i_left, i_right, index_map):
+        def helper(left, right, index_map):
             
-            if i_left > i_right:
+            if left > right:
                 return None
             
-            root = TreeNode(preorder.pop(0))
+            root = Node(preorder.pop(0))
             index_here = index_map[root.val]
 
             # this order change from postorder
-            root.left = helper(i_left, index_here - 1, index_map)
-            root.right = helper(index_here + 1, i_right, index_map)
+            root.left = helper(left, index_here - 1, index_map)
+            root.right = helper(index_here + 1, right, index_map)
             
             return root
-            
         
-        index_map = {value: index for index, value in enumerate(inorder)}
+        index_map = {value: i for i, value in enumerate(inorder)}
         
         return helper(0, len(inorder) - 1, index_map)
         
