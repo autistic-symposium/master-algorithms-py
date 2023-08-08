b14d9fcee2bb31e641686b27e58a3fafe56917c8
marina <138340846+bt3gl-cryptographer@users.noreply.github.com>
2023-08-07 17:31:39 -0700

    Update is_anagram.py

--- arrays_and_strings/is_anagram.py
+++ arrays_and_strings/is_anagram.py
@@ -11,16 +11,8 @@ def is_anagram(string1, string2) -> bool:
     if len(string1) != len(string2):
         return False
 
-    for char in string1:
-        if char not in string2:
+    for c in string1:
+        if c not in string2:
             return False
     
     return True
-
-
-if __name__ == '__main__':
-    
-    print('Testing is_anagram()...')
-    string1 = "listen"
-    string2 = "silent"
-    print(f'Is {string1} an anagram of {string2}?: {is_anagram(string1, string2)}')
