153e85ca03bbf744fe720d3a5738e042f173ffdc
bt3gl <138340846+bt3gl-cryptographer@users.noreply.github.com>
2023-08-08 13:52:20 -0700

    Update bt_is_same_trees.py

--- trees/bt_is_same_trees.py
+++ trees/bt_is_same_trees.py
@@ -11,4 +11,4 @@ def is_same_trees(p, q):
         if (not p and q) or (not q and p) or p.val != q.val:
             return False
         
-        return self.is_same_trees(p.right, q.right) and self.is_same_trees(p.left, q.left)
+        return is_same_trees(p.right, q.right) and is_same_trees(p.left, q.left)
