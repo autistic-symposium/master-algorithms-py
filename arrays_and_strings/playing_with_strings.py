e54535ae2bb93becdc45493707b3bb4329d3d322
marina <138340846+bt3gl-cryptographer@users.noreply.github.com>
2023-08-07 17:35:27 -0700

    Update playing_with_strings.py

--- arrays_and_strings/playing_with_strings.py
+++ arrays_and_strings/playing_with_strings.py
@@ -3,13 +3,4 @@
 # author: bt3gl
 
 def reverse_array_in_place(array):
-    """ Reverse an array in place """
     return array[::-1]
-
-
-if __name__ == '__main__':
-
-    print('Testing reverse_array_in_place')
-    array = [1, 2, 3, 4, 5]
-    print(f'Array: {array}')
-    print(f'Reversed: {reverse_array_in_place(array)}')
