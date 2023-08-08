15d407b4efc080715f1c6f93dc658181c6404fc2
bt3gl <138340846+bt3gl-cryptographer@users.noreply.github.com>
2023-08-08 14:27:07 -0700

    Update bt_inorder.py

--- trees/bt_inorder.py
+++ trees/bt_inorder.py
@@ -2,10 +2,30 @@
 # -*- coding: utf-8 -*-
 # author: bt3gl
 
+
 def inorder(root) -> list:
         
   if root is None:
       return []
     
   return inorder(root.left) + [root.val] + inorder(root.right)
+        
+
+def inorder_iterative(root) -> list:
+        
+        result = []
+        stack = []
+        node = root
+        
+        while stack or node:
+
+            if node:
+		stack.append(node)
+                node = node.left
+            else: 
+                node = stack.pop()
+                result.append(node.val)
+                node = node.right
+            
+        return result
         
