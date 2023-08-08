94de93780af921d4551f6bfff3ad27e9368c194b
bt3gl <138340846+bt3gl-cryptographer@users.noreply.github.com>
2023-08-08 14:55:51 -0700

    Update bt_count_unival.py

--- trees/bt_count_unival.py
+++ trees/bt_count_unival.py
@@ -2,29 +2,20 @@
 # -*- coding: utf-8 -*-
 # author: bt3gl
 
-'''
-Given the root of a binary tree, return the number of uni-value subtrees.
-A uni-value subtree means all nodes of the subtree have the same value.
-'''
+
  
-def count_unival(root: Optional[TreeNode]) -> int:
+def count_unival(root) -> int:
         
         global count = 0
         
         def dfs(node):
-            
-            if not node:
+            if node is None:
                 return True
-        
-            is_uni_left = dfs(node.left)
-            is_uni_right = dfs(node.right)
             
-            if is_uni_left and is_uni_right:
-                if node.left and node.left.val != node.val:
-                    return False
-                if node.right and node.right.val != node.val:
-                    return False
-            
+            if dfs(node.left) and dfs(node.right):
+                if (node.left and node.left.val != node.val) or \
+                    (node.right and node.right.val != node.val):
+                         return False
                 self.count += 1
                 return True
             
