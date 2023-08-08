b9b5d8e21c1fe5bc0ea43fc690f4325c39f644d4
marina <138340846+bt3gl-cryptographer@users.noreply.github.com>
2023-08-07 17:36:23 -0700

    Update and rename str-longest-non-repeating.py to longest_non_repeating.py

--- arrays_and_strings/longest_non_repeating.py
+++ arrays_and_strings/longest_non_repeating.py
@@ -2,21 +2,16 @@
 # -*- coding: utf-8 -*-
 # author: bt3gl
 
-# find the length of the longest substring without repeating characters
 
-
-def length_longest_substring(s: str) -> int:
+def length_longest_substring(s) -> int:
         
         result = ""
         this_longest_string = ""
-        
         i = 0
         
         for c in s:
-            
             j = 0
         
-            # this loop breaks if repeated
             while j < len(this_longest_string):
                 
                 if c == this_longest_string[j]:
@@ -26,17 +21,7 @@ def length_longest_substring(s: str) -> int:
                     
                 j += 1
             
-            # this loop continues creating the string
             this_longest_string += c
-                
             
         return result, this_longest_string
-            
-                
-
-if __name__ == "__main__":
-    s = "abcabcbb"
-    print(length_longest_substring(s))
-
-    s = "dvdf"
-    print(length_longest_substring(s))
+        
