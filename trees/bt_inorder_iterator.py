ef073d11cad6b1fb931a3d919dc41a2ee3ccde72
bt3gl <138340846+bt3gl-cryptographer@users.noreply.github.com>
2023-08-08 15:37:13 -0700

    Update and rename bst_inorder_iterator.py to bt_inorder_iterator.py

--- trees/bt_inorder_iterator.py
+++ trees/bt_inorder_iterator.py
@@ -8,29 +8,24 @@ class Node:
       self.left = left
       self.right = right
 
+
 class BST_Iterator:
 
-    def __init__(self, root: Optional[Node]):
-        
+    def __init__(self, root):
         self.stack = []
         self.left_inorder(root)
         
     def left_inorder(self, root):
-        
         while root:
             self.stack.append(root)
             root = root.left
 
     def next(self) -> int:
-        
         top_node = self.stack.pop()
-        
         if top_node.right:
             self.left_inorder(top_node.right)
-        
         return top_node.val
         
     def has_next(self) -> bool:
-        
         return len(self.stack) > 0
         
